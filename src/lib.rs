8324b9fcbde12cbefc73f8ebe81f9734b56bf24e
Tom Anderson <tom@ando.gq>
2022-07-15 22:05:10 +1000

    Convert to handlebars templating
    Close #7

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,21 @@
 use github::{File, Repo};
+use handlebars::Handlebars;
 use pulldown_cmark::{Options, Parser};
-use utils::replace;
+use serde_json::json;
 use worker::*;
 
 mod github;
 mod utils;
 
-enum ContentType {
-    String(String),
-    List(Vec<Vec<(String, String)>>),
-}
-
 async fn generate_page(
     repo: &Repo,
     page: &str,
     title: &str,
-    content: Vec<(String, ContentType)>,
+    content: serde_json::Value,
 ) -> Option<String> {
     // Download template and core template from github
     // TODO Could make these in parallel
-    if let (Some(mut core_template), Some(mut page_template)) = (
+    if let (Some(core_template), Some(page_template)) = (
         File::new(&repo.get_contents_path("/templates/core.html"))
             .get_contents()
             .await,
@@ -27,48 +23,22 @@ async fn generate_page(
             .get_contents()
             .await,
     ) {
-        // Fill content
-        for (key, value) in content.iter() {
-            page_template = match value {
-                ContentType::String(value) => replace(page_template, key, value),
-                ContentType::List(iterations) => {
-                    if let (Some(template_start), Some(template_end)) = (
-                        page_template.find(&format!("{{{{{}}}}}", key)),
-                        page_template
-                            .find(&format!("{{{{/{}}}}}", key))
-                            .map(|i| i + key.len() + 5),
-                    ) {
-                        let section_template = page_template
-                            [(template_start + key.len() + 4)..(template_end - (key.len() + 5))]
-                            .to_owned();
-                        page_template.replace_range(
-                            template_start..template_end,
-                            &iterations
-                                .iter()
-                                .map(|replacements| {
-                                    replacements.iter().fold(
-                                        section_template.to_owned(),
-                                        |template, (key, value)| replace(template, key, value),
-                                    )
-                                })
-                                .collect::<Vec<String>>()
-                                .join("\n"),
-                        );
-                        page_template
-                    } else {
-                        page_template
-                    }
-                }
+        let handlebars = Handlebars::new();
+
+        if let Ok(html) = handlebars.render_template(&page_template, &content) {
+            if let Ok(html) = handlebars.render_template(
+                &core_template,
+                &json!({
+                    "title": title,
+                    "content": html
+                }),
+            ) {
+                return Some(html);
             }
         }
-
-        core_template = replace(core_template, "content", &page_template);
-        core_template = replace(core_template, "title", title);
-
-        Some(core_template)
-    } else {
-        None
     }
+
+    None
 }
 
 fn log_request(req: &Request) {
@@ -92,58 +62,34 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
             if let Some(user) = github::User::get("andogq").await {
                 if let Some(repo) = user.get_repo("blog".to_string()).await {
                     let issues = repo.get_issues().await;
-                    let posts = issues
-                        .iter()
-                        .map(|post| {
-                            format!("<li><a href=\"/posts/{}\">{}</a></li>", post.id, post.title)
-                        })
-                        .collect::<String>();
 
                     let pinned_repositories = user
                         .get_pinned(&env.secret("gh_key").unwrap().to_string())
                         .await;
 
-                    let mut content: Vec<(String, ContentType)> = vec![
-                        ("name", &user.name as &str),
-                        ("profile_picture", &user.profile_picture),
-                        ("bio", &user.bio),
-                        ("hireable", if user.hireable { "yes" } else { "no" }),
-                        ("posts", &posts),
-                    ]
-                    .iter()
-                    .map(|(key, value)| (key.to_string(), ContentType::String(value.to_string())))
-                    .collect();
+                    let content = json!({
+                        "name": user.name,
+                        "profile_picture": user.profile_picture,
+                        "bio": user.bio,
+                        "hireable": if user.hireable { "yes" } else { "no" },
+                        "posts": issues.iter().map(|post| json!({
+                            "link": format!("/posts/{}", post.id),
+                            "title": post.title
+                        })).collect::<Vec<serde_json::Value>>(),
+                        "pinned": pinned_repositories.iter().map(|repo| json!({
+                            "name": repo.name,
+                            "description": repo.description,
+                            "languages": repo.languages.join(", ")
+                        })).collect::<Vec<serde_json::Value>>()
+                    });
 
-                    content.push((
-                        "pinned".to_string(),
-                        ContentType::List(
-                            pinned_repositories
-                                .into_iter()
-                                .map(|repo| {
-                                    vec![
-                                        ("repo.name".to_string(), repo.name),
-                                        (
-                                            "repo.description".to_string(),
-                                            repo.description.unwrap_or_else(|| "".to_string()),
-                                        ),
-                                        ("repo.languages".to_string(), repo.languages.join(", ")),
-                                    ]
-                                })
-                                .collect(),
-                        ),
-                    ));
-
-                    if let Some(page) = generate_page(&repo, "home", &user.name, content).await {
-                        Response::from_html(page)
-                    } else {
-                        Response::error("Internal error", 500)
+                    if let Some(html) = generate_page(&repo, "home", &user.name, content).await {
+                        return Response::from_html(html);
                     }
-                } else {
-                    Response::error("Not found", 404)
                 }
-            } else {
-                Response::error("Internal error", 500)
             }
+
+            Response::error("Internal error", 500)
         })
         .get_async("/posts/:id", |_, ctx| async move {
             if let Ok(id) = ctx.param("id").unwrap().parse::<u32>() {
@@ -159,38 +105,22 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
                             let mut rendered = String::new();
                             pulldown_cmark::html::push_html(&mut rendered, parser);
 
-                            let content: Vec<(&str, &str)> =
-                                vec![("post_title", &issue.title), ("post_body", &rendered)];
+                            let content = json!({
+                                "post_title": issue.title,
+                                "post_body": rendered
+                            });
 
-                            if let Some(page) = generate_page(
-                                &repo,
-                                "post",
-                                &issue.title,
-                                content
-                                    .iter()
-                                    .map(|(key, value)| {
-                                        (key.to_string(), ContentType::String(value.to_string()))
-                                    })
-                                    .collect(),
-                            )
-                            .await
+                            if let Some(page) =
+                                generate_page(&repo, "post", &issue.title, content).await
                             {
-                                Response::from_html(page)
-                            } else {
-                                Response::error("Internal error", 500)
+                                return Response::from_html(page);
                             }
-                        } else {
-                            Response::error("Not found", 404)
                         }
-                    } else {
-                        Response::error("Not found", 404)
                     }
-                } else {
-                    Response::error("Internal error", 500)
                 }
-            } else {
-                Response::error("Internal error", 500)
             }
+
+            Response::error("Internal error", 500)
         })
         .run(req, env)
         .await
