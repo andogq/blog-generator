acd771723b97f6560d15decc764c76946eac368f
Tom Anderson <tom@ando.gq>
2022-08-29 11:52:21 +1000

    Deal with some build issues

--- webapp/src/lib/jwt.ts
+++ webapp/src/lib/jwt.ts
@@ -1,4 +1,4 @@
-import jose from "jose";
+import * as jose from "jose";
 
 const JWT_SECRET = process.env["JWT_SECRET"] || "";
 if (!JWT_SECRET) throw new Error("JWT_SECRET not found in environment");
