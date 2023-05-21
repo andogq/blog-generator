ada292830c674e860c714f94e328d5ca23a1e89c
Tom Anderson <tom@ando.gq>
2023-05-21 22:41:18 +1000

    Remove build artifacts

--- backend/target/debug/deps/backend-3a35c8950c6e167a.d
+++
@@ -1,8 +0,0 @@
-/Users/tom/code/github_blog/backend/target/debug/deps/backend-3a35c8950c6e167a.rmeta: src/main.rs Cargo.toml
-
-/Users/tom/code/github_blog/backend/target/debug/deps/backend-3a35c8950c6e167a.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
